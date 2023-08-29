6217b414a13f6a35487f9713a8f714a932674fc9
yeonso08 <hjng0825@gmail.com>
2023-08-29 17:52:35 +0900

    feat : NewArrivals에 grid 추가

--- src/pages/Home/NewArrivals.jsx
+++ src/pages/Home/NewArrivals.jsx
@@ -1,3 +1,36 @@
-export default function NewArrivals() {
-    return;
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+
+function NewArrivals() {
+  return (
+    <Container fluid>
+      <Row className="justify-content-between">
+        <Col md="auto">
+          <div>New Arrivals</div>
+        </Col>
+        <Col md="auto">
+          <div>View All 〉</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card style={{ width: "18rem", border: "none" }}>
+            <Card.Img variant="top" src="./Bag1.png" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
+export default NewArrivals;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import Layout from "../../components/Layout/Layout";
 import Hero from "./Hero";
+import NewArrivals from "./NewArrivals";
 
 export default function Home() {
     return (
         <>
             <Layout />
             <Hero />
+            <NewArrivals />
         </>
     )
 }
