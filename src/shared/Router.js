5476819be413205d9b549888ea119c8fb6185b75
yeonso08 <hjng0825@gmail.com>
2023-08-29 10:43:09 +0900

    feat : connect home

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* <Route path = "/" element={} /> */}
+                <Route path = "/" element={<Home />} />
                 <Route path = "/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
