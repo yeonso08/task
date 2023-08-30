e4a57e534381098beaa87ea07ee0db238deb29b0
yeonso08 <hjng0825@gmail.com>
2023-08-30 11:44:11 +0900

    feat : react-icons 라이브러리 추가 하트 아이콘 추가 및 적용

--- src/pages/Home/NewArrivals.jsx
+++ src/pages/Home/NewArrivals.jsx
@@ -6,10 +6,12 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import axios from "axios";
 import "./Home.css"; // CSS 파일을 import 합니다.
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 function NewArrivals() {
   const [products, setProducts] = useState([]);
-  const scrollableRef = useRef(null);  // 1. 스크롤을 조정할 ref 생성
+  const [likedProducts, setLikedProducts] = useState({}); // 각 제품의 좋아요 상태를 저장
+  const scrollableRef = useRef(null); // 1. 스크롤을 조정할 ref 생성
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,11 +26,19 @@ function NewArrivals() {
     fetchData();
   }, []);
 
+  const toggleLike = (productId) => {
+    setLikedProducts((prevState) => ({
+      ...prevState,
+      [productId]: !prevState[productId],
+    }));
+  };
+
   useEffect(() => {
     const scrollInterval = setInterval(() => {
       if (scrollableRef.current) {
         const currentScrollPosition = scrollableRef.current.scrollLeft;
-        const maxScroll = scrollableRef.current.scrollWidth - scrollableRef.current.clientWidth;  // 스크롤 가능한 최대 위치를 계산
+        const maxScroll =
+          scrollableRef.current.scrollWidth - scrollableRef.current.clientWidth; // 스크롤 가능한 최대 위치를 계산
 
         if (currentScrollPosition >= maxScroll) {
           // 스크롤이 최대 위치에 도달하면, 다시 시작으로 돌립니다.
@@ -43,7 +53,6 @@ function NewArrivals() {
     return () => clearInterval(scrollInterval);
   }, []);
 
-
   return (
     <Container fluid>
       <Row className="justify-content-between">
@@ -54,18 +63,40 @@ function NewArrivals() {
           <div>View All 〉</div>
         </Col>
       </Row>
-      <Row className="scrollableRow" ref={scrollableRef}>  {/* ref 설정 */}
+      <Row className="scrollableRow" ref={scrollableRef}>
         {products.map((item) => (
-          <Col key={item.id} md={4} lg={3} xl={2}>
+          <Col
+            key={item.id}
+            md={4}
+            lg={3}
+            xl={2}
+            style={{ marginRight: "40px" }}
+          >
             <Card style={{ width: "18rem", height: "500px", border: "none" }}>
               <Card.Img
                 variant="top"
                 src={item.image}
-                style={{ height: "286px" }}
+                style={{ height: "286px", backgroundColor: "#C4C4C4" }}
               />
               <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>{item.category}</Card.Text>
+                <Card.Title style={{ fontSize: "18px" }}>
+                  {item.title}
+                </Card.Title>
+                <Card.Subtitle> {item.category}</Card.Subtitle>
+                <Card.Text>
+                  ${item.price}
+                  {likedProducts[item.id] ? (
+                    <AiFillHeart
+                      style={{ float: "right", color: "red" }}
+                      onClick={() => toggleLike(item.id)}
+                    />
+                  ) : (
+                    <AiOutlineHeart
+                      style={{ float: "right" }}
+                      onClick={() => toggleLike(item.id)}
+                    />
+                  )}
+                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
