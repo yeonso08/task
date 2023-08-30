5253e79faf3ee4a6e7de8f7a586e3324820f26f6
yeonso08 <hjng0825@gmail.com>
2023-08-30 10:45:19 +0900

    feat : NewArrivals 자동 슬라이드 기능 추가

--- src/pages/Home/NewArrivals.jsx
+++ src/pages/Home/NewArrivals.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
@@ -9,6 +9,7 @@ import "./Home.css"; // CSS 파일을 import 합니다.
 
 function NewArrivals() {
   const [products, setProducts] = useState([]);
+  const scrollableRef = useRef(null);  // 1. 스크롤을 조정할 ref 생성
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,6 +24,26 @@ function NewArrivals() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const scrollInterval = setInterval(() => {
+      if (scrollableRef.current) {
+        const currentScrollPosition = scrollableRef.current.scrollLeft;
+        const maxScroll = scrollableRef.current.scrollWidth - scrollableRef.current.clientWidth;  // 스크롤 가능한 최대 위치를 계산
+
+        if (currentScrollPosition >= maxScroll) {
+          // 스크롤이 최대 위치에 도달하면, 다시 시작으로 돌립니다.
+          scrollableRef.current.scrollLeft = 0;
+        } else {
+          // 아니면 스크롤을 계속 진행합니다.
+          scrollableRef.current.scrollLeft = currentScrollPosition + 1;
+        }
+      }
+    }, 30);
+
+    return () => clearInterval(scrollInterval);
+  }, []);
+
+
   return (
     <Container fluid>
       <Row className="justify-content-between">
@@ -33,7 +54,7 @@ function NewArrivals() {
           <div>View All 〉</div>
         </Col>
       </Row>
-      <Row className="scrollableRow">
+      <Row className="scrollableRow" ref={scrollableRef}>  {/* ref 설정 */}
         {products.map((item) => (
           <Col key={item.id} md={4} lg={3} xl={2}>
             <Card style={{ width: "18rem", height: "500px", border: "none" }}>
