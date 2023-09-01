06bec1c34bcfebbd0d2585223d9292401b544f80
yeonso08 <hjng0825@gmail.com>
2023-09-01 09:56:03 +0900

    feat : cart 페이지 생성, 라우터 연결

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,6 +3,7 @@ import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Category from "../pages/Category";
 import Detail from "../pages/Detail";
+import Cart from "../pages/Cart";
 
 export default function Router() {
   return (
@@ -12,6 +13,7 @@ export default function Router() {
         <Route path="/login" element={<Login />} />
         <Route path="/category/:id" element={<Category />} />
         <Route path="/products/:id" element={<Detail />} />
+        <Route path="/carts" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
