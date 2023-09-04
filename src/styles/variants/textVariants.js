c065ac53eba39184ddf7fdf5383cb75f715df287
yeonso08 <hjng0825@gmail.com>
2023-09-04 09:41:00 +0900

    feat : textVariants 생성 및 추가

---
+++ src/styles/variants/textVariants.js
@@ -0,0 +1,64 @@
+const textVariants = {
+  H_B_52: {
+    fontSize: "var(--font-size-xl)",
+    fontWeight: "var(--font-weight-bold)",
+    lineHeight: "var(--line-height-xl)",
+  },
+  H_S_34: {
+    fontSize: "var(--font-size-lg)",
+    fontWeight: "var(--font-weight-semibold)",
+    lineHeight: "var(--line-height-lg)",
+  },
+  H_S_20: {
+    fontSize: "var(--font-size-md)",
+    fontWeight: "var(--font-weight-semibold)",
+    lineHeight: "var(--line-height-md)",
+  },
+  H_M_14: {
+    fontSize: "var(--font-size-xs)",
+    fontWeight: "var(--font-weight-medium)",
+    lineHeight: "var(--line-height-xs)",
+  },
+  H_S_14: {
+    fontSize: "var(--font-size-xs)",
+    fontWeight: "var(--font-weight-semibold)",
+    lineHeight: "var(--line-height-xxs)",
+  },
+  H_M_16: {
+    fontSize: "var(--font-size-sm)",
+    fontWeight: "var(--font-weight-medium)",
+    lineHeight: "var(--line-height-sm)",
+  },
+  P_R_16: {
+    fontSize: "var(--font-size-sm)",
+    fontWeight: "var(--font-weight-regular)",
+    lineHeight: "var(--line-height-sm)",
+  },
+  P_M_16: {
+    fontSize: "var(--font-size-sm)",
+    fontWeight: "var(--font-weight-medium)",
+    lineHeight: "var(--line-height-sm)",
+  },
+  P_R_14: {
+    fontSize: "var(--font-size-xs)",
+    fontWeight: "var(--font-weight-regular)",
+    lineHeight: "var(--line-height-sm)",
+  },
+  P_M_12: {
+    fontSize: "var(--font-size-xxs)",
+    fontWeight: "var(--font-weight-medium)",
+    lineHeight: "var(--line-height-sm)",
+  },
+  P_S_12: {
+    fontSize: "var(--font-size-xxs)",
+    fontWeight: "var(--font-weight-semibold)",
+    lineHeight: "var(--line-height-xxs)",
+  },
+  C_M_12: {
+    fontSize: "var(--font-size-xxs)",
+    fontWeight: "var(--font-weight-medium)",
+    lineHeight: "var(--line-height-xs)",
+  },
+};
+
+export default textVariants;
