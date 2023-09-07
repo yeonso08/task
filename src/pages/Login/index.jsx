3c7bc27ea197b81ccbdfc03eedc342bb2ece9cad
yeonso08 <hjng0825@gmail.com>
2023-09-07 09:28:08 +0900

    feat : 로그인 기능 구현 및 적용

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,55 @@
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
 
 export default function Login() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('https://fakestoreapi.com/auth/login', {
+        username: id,
+        password: password
+      });
+
+      if (response.data && response.data.token) {
+        console.log("Login successful!", response.data.token);
+      } else {
+        console.error("Login failed!");
+      }
+    } catch (error) {
+      console.error("Error during login: ", error);
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formGroupEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="id"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+          placeholder="Enter email"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formGroupPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Password"
+        />
       </Form.Group>
+      <button type="submit">Login</button>
     </Form>
   );
 }
